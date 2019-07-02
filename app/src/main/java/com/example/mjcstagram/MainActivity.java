e0b2b0d9c5f4f79dda4c54ced89a8e0db473e715
leemJungHun <leem559@naver.com>
2019-07-02 17:29:04 +0900

    fragment시작

--- app/src/main/java/com/example/mjcstagram/MainActivity.java
+++ app/src/main/java/com/example/mjcstagram/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.mjcstagram;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import com.example.mjcstagram.databinding.Main;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    Main binding;
      @Override
     protected void onCreate(Bundle savedInstanceState) {
          super.onCreate(savedInstanceState);
-         setContentView(R.layout.activity_main);
+         binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
+         binding.setActivity(this);
+
      }
+    public void homeFragment(View view) {
+
+    }
+    public void searchFragment(View view) {
+
+    }
+    public void likeFragment(View view) {
+
+    }
+    public void myFragment(View view) {
+
+    }
+
+
 }
